751f47a96af984a9a739c9e19249ad1abe77f83f
vitorio19942 <vitorio19942@gmail.com>
2025-07-23 02:20:14 +0000

    ne

--- backend/config.py
+++ backend/config.py
@@ -7,11 +7,11 @@ load_dotenv() # Carrega variáveis do arquivo .env
 class Config:
     # Configurações básicas do Flask
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'uma-chave-secreta-padrao-muito-segura' # Altere em produção!
-    # ... outras configurações do Flask ...
 
     # Configurações do SQLAlchemy
+    # ADICIONE AQUI O PARAMETRO 'client_encoding=latin1'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        f"postgresql://{os.environ.get('DB_USER')}:{os.environ.get('DB_PASSWORD')}@{os.environ.get('DB_HOST')}/{os.environ.get('DB_NAME')}?sslmode=require"
+        f"postgresql+psycopg2://{os.environ.get('DB_USER')}:{os.environ.get('DB_PASSWORD')}@{os.environ.get('DB_HOST')}/{os.environ.get('DB_NAME')}?sslmode=require&client_encoding=latin1"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     # Configurações específicas da aplicação (se houver)
